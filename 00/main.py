04f98a691d557b648cba2444dde3c310ba0e300d
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2023-06-13 21:35:08 -0400

    add logging

--- 00/main.py
+++ 00/main.py
@@ -1,13 +1,15 @@
 import socket
 import threading
+import logging
 
-host = '0.0.0.0'
-port = 21234
-address = (host, port)
+logging.basicConfig(level=logging.DEBUG)
+HOST = '0.0.0.0'
+PORT = 21234
 
-def magic(conn: socket.socket):
+def handler(conn: socket.socket, addr: tuple[str, int], thread_name: str):
+    global i 
     with conn:
-        print(f"Connection with {addr} established")
+        logging.info(f"Connection with {addr} established on thread {thread_name}")
         while True: 
             data = conn.recv(1024)
 
@@ -15,17 +17,25 @@ def magic(conn: socket.socket):
                 break
 
             conn.sendall(data)
+        logging.info(f"Connection with {addr} on thread {thread_name} closed")
+        i -= 1
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(address)
+if __name__ == '__main__':
 
-    s.listen(5)
-    while True:
-        conn, addr = s.accept()
-        print(f'accepted {addr}')
-        t = threading.Thread(target=magic, args=(conn,))
-        t.start()
+    address = (HOST, PORT)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(address)
+
+        s.listen(5)
+        i = 1
+        while True:
+            conn, addr = s.accept()
+            thread_name = f't{i}'
+            i += 1
+            t = threading.Thread(target=handler, args=(conn, addr, thread_name))
+            t.start()
 
 
 
